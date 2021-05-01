c9ac5279ff808b2efa820361b156b0e7395649e4
tarek99samy <tarek.saad99@eng-st.cu.edu.eg>
2021-05-01 02:51:50 +0200

    setting up eslint and prettier and their configs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./styles/reset.scss";
+import React from 'react';
+import './styles/reset.scss';
 
 export default function App() {
-	return (
-		<div className="App">
-			<h1>Hello</h1>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <h1>Hello</h1>
+    </div>
+  );
 }
