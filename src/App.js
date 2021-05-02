31b77ae48d254dd2afa65275add1b1fd2ea6851d
tarek99samy <tarek.saad99@eng-st.cu.edu.eg>
2021-05-02 23:06:37 +0200

    replacing material with bootstrap , setting context and private routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/private_route/private_route';
+import { UserContextProvider } from './context/user_context';
+import Login from './pages/login/login';
+import SignUp from './pages/signup/signup';
+import NotFound from './pages/not_found/not_found';
 import './styles/reset.scss';
 
 export default function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <UserContextProvider>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={SignUp} />
+          {/* to use private routes, delete this comment and put your code here
+						<PrivateRoute exact path="/...">
+							<Component />
+						</PrivateRoute>
+					*/}
+          <Route exact path='/notfound' component={NotFound} />
+          <Redirect to='/notfound' />
+        </UserContextProvider>
+      </Switch>
+    </BrowserRouter>
   );
 }
