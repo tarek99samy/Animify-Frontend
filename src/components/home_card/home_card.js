50f6c1d65ec72bd1f187a1cc9f1ac6c468a96952
EmanOthman21 <eothman21@gmail.com>
2021-05-09 03:33:12 +0200

    Edit: Sass usage , use spans

--- src/components/home_card/home_card.js
+++ src/components/home_card/home_card.js
@@ -5,16 +5,16 @@ import './home_card.scss';
 const HomeCard = (props) => {
   return (
     <div className='container-fluid'>
-      <div className='d-flex justify-content-between mb-3'>
-        <h4 className='section__name p-2'>{props.name}</h4>
-        <div className='show__more p-2'>View More</div>
+      <div className='d-flex justify-content-between'>
+        <span className='section__name ml-4'>{props.name}</span>
+        <span className='show__more'>View More</span>
       </div>
       <div className='row home_cards card-group'>
         {props.list.map((anime) => (
           <div className='card home__card'>
             <img src={anime.link} className='card__img' alt='animeImg' />
             <div className='card__body'>
-              <p className='card__body-txt'>{anime.name}</p>
+              <span className='card__body-txt'>{anime.name}</span>
             </div>
           </div>
         ))}
