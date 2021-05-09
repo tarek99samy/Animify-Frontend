82c2d3e85e7fbe932821b021bd314d495aa2d391
EmanOthman21 <eothman21@gmail.com>
2021-05-09 03:46:45 +0200

    Delete App code, Edit BEM usage

--- src/components/home_card/home_card.js
+++ src/components/home_card/home_card.js
@@ -4,17 +4,17 @@ import './home_card.scss';
 
 const HomeCard = (props) => {
   return (
-    <div className='container-fluid'>
+    <div className='container-fluid home-cards'>
       <div className='d-flex justify-content-between'>
-        <span className='section__name ml-4'>{props.name}</span>
-        <span className='show__more'>View More</span>
+        <span className='home-cards__title ml-4'>{props.name}</span>
+        <span className='home-cards__more'>View More</span>
       </div>
-      <div className='row home_cards card-group'>
+      <div className='row home-cards__wrapper card-group'>
         {props.list.map((anime) => (
-          <div className='card home__card'>
-            <img src={anime.link} className='card__img' alt='animeImg' />
-            <div className='card__body'>
-              <span className='card__body-txt'>{anime.name}</span>
+          <div className='card home-card'>
+            <img src={anime.link} className='home-card__img' alt='animeImg' />
+            <div>
+              <span className='home-card__txt'>{anime.name}</span>
             </div>
           </div>
         ))}
