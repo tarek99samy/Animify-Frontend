44a0edcc50b964b55ab0ef8445274d96722d68b1
EmanOthman21 <eothman21@gmail.com>
2021-05-21 23:14:45 +0200

    Remove unwanted components from App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Login from './pages/login/login';
 import SignUp from './pages/signup/signup';
 import NotFound from './pages/not_found/not_found';
 import './styles/reset.scss';
-import SideBar from './components/side_bar/side_bar';
-import NavBar from './components/nav_bar/nav_bar';
 
 export default function App() {
   return (
@@ -16,8 +14,6 @@ export default function App() {
         <UserContextProvider>
           <Route exact path='/login' component={Login} />
           <Route exact path='/signup' component={SignUp} />
-          <SideBar />
-          <NavBar />
           {/* <Route exact path='/notfound' component={NotFound} />
           <Redirect to='/notfound' /> */}
         </UserContextProvider>
