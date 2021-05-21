b1ce1f7936dc8b6a8934ab69d41d5684f0c817b9
EmanOthman21 <eothman21@gmail.com>
2021-05-21 15:19:44 +0200

    Add responsive navbar with search input

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Login from './pages/login/login';
 import SignUp from './pages/signup/signup';
 import NotFound from './pages/not_found/not_found';
 import './styles/reset.scss';
+import SideBar from './components/side_bar/side_bar';
+import NavBar from './components/nav_bar/nav_bar';
 
 export default function App() {
   return (
@@ -14,7 +16,8 @@ export default function App() {
         <UserContextProvider>
           <Route exact path='/login' component={Login} />
           <Route exact path='/signup' component={SignUp} />
-
+          <SideBar />
+          <NavBar />
           {/* <Route exact path='/notfound' component={NotFound} />
           <Redirect to='/notfound' /> */}
         </UserContextProvider>
