f63d11e58187f80c1157dde4dffdea616cef2191
EmanOthman21 <eothman21@gmail.com>
2021-05-23 16:06:46 +0200

    Fix Cards

--- src/components/home_card/home_card.js
+++ src/components/home_card/home_card.js
@@ -13,15 +13,13 @@ const HomeCard = (props) => {
       </div>
       <div className='row home-cards__wrapper card-group'>
         {props.list.map((anime, index) => (
-          <Link to={`/anime-info/0/${anime.gotoURL}`}>
-            <fragment>
-              <div className='card home-card' key={index}>
-                <img src={anime.artwork} className='home-card__img card-img-top fluid-img' alt='animeImg' />
-                <div>
-                  <span className='home-card__txt'>{anime.name}</span>
-                </div>
+          <Link to={`/anime-info/0/${anime.gotoURL}`} className='home-card card'>
+            <div key={index}>
+              <img src={anime.artwork} className='home-card__img card-img-top fluid-img' alt='animeImg' />
+              <div>
+                <span className='home-card__txt'>{anime.name}</span>
               </div>
-            </fragment>
+            </div>
           </Link>
         ))}
       </div>
