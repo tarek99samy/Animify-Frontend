28b59ab653e03f12c6aa15ab73b0499ad9caa4eb
EmanOthman21 <eothman21@gmail.com>
2021-05-26 23:18:23 +0200

    Add Logout button without calling the function

--- src/components/side_bar/side_bar.js
+++ src/components/side_bar/side_bar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import React, { useEffect, useState } from 'react';
+import { isLoggedIn } from '../../utils/state_manager';
 import SideBarElement from './side_bar_element';
 import './side_bar.scss';
 
@@ -30,6 +32,7 @@ const sideBarList = items.map((item, index) => {
   return <SideBarElement item={item} key={index} />;
 });
 function SideBar() {
+
   return (
     <div className='sidebar flex-column'>
       <div>
@@ -37,7 +40,18 @@ function SideBar() {
           <h4 className='sidebar__title'>Animify</h4>
         </Link>
       </div>
-      <ul className='nav flex-column sidebar__list'>{sideBarList}</ul>
+      <ul className='nav flex-column sidebar__list'>
+        {sideBarList}
+        { isLoggedIn()?(
+
+          <li className='nav-item sidebar__item'>
+          <Link to='/' className='nav-link sidebar__item__link'>
+            <i className='fa fa-sign-out-alt sidebar__item__icon'></i>
+            <span className='sidebar__item__text'>Logout</span>
+          </Link>
+        </li>
+        ):null}
+      </ul>
     </div>
   );
 }
