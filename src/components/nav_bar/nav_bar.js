8b61501e88b7b6bdfa0f6d833ab77ea7c8af99e0
EmanOthman21 <eothman21@gmail.com>
2021-05-27 11:55:06 +0200

    Add navbar logged out section

--- src/components/nav_bar/nav_bar.js
+++ src/components/nav_bar/nav_bar.js
@@ -9,7 +9,7 @@ function NavBar() {
       <nav className='navbar'>
         <input type='search' className='navbar__search' placeholder='Search for animes' />
         {isLoggedIn() ? (
-          <div className='navbar__buttons'>
+          <div className='navbar__buttons--login'>
             <Link to='/profile'>
               <span className='navbar__username'>Eman Othman</span>
             </Link>
@@ -18,7 +18,20 @@ function NavBar() {
             </Link>
             <i className='fa fa-bell navbar__icon fa-lg'></i>
           </div>
-        ) : null}
+        ) : (
+          <div className='navbar__buttons--logout'>
+            <Link to='/login'>
+              <button type='button' className='btn navbar__loginbtn'>
+                Log in
+              </button>
+            </Link>
+            <Link to='/signup'>
+              <button type='button' className='btn navbar__signbtn'>
+                Sign Up
+              </button>
+            </Link>
+          </div>
+        )}
       </nav>
     </div>
   );
