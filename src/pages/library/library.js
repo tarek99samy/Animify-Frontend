0e16e525841a93a830ec02d6e75c877cea08ab6a
EmanOthman21 <eothman21@gmail.com>
2021-06-08 16:49:45 +0200

    Add clicked search request

--- src/pages/library/library.js
+++ src/pages/library/library.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import LibraryLogout from '../../components/library/library_logout';
 import LibraryLogin from '../../components/library/library_login';
-import { isLoggedIn, getUserToken } from '../../utils/state_manager';
+import { isLoggedIn } from '../../utils/state_manager';
 import './library.scss';
 
 function Library() {
