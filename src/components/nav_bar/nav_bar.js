30a46721cdbb5f4e212b0a99dfc9b257ea492fe3
EmanOthman21 <eothman21@gmail.com>
2021-06-08 07:18:44 +0200

    Add search suggestions layout

--- src/components/nav_bar/nav_bar.js
+++ src/components/nav_bar/nav_bar.js
@@ -7,10 +7,12 @@ import './nav_bar.scss';
 function NavBar() {
   const [searchQuery, setSearchQuery] = useState('');
   const [hideValue, setHideValue] = useState('');
+  const [searchFoucs, setSearchFocus] = useState('');
+
   const history = useHistory();
   const location = useLocation();
   const handleClickOnSearch = (event) => {
-    if (event.charCode == 13) {
+    if (event.charCode === 13) {
       history.replace({ pathname: `/search-result/0/${searchQuery}` });
     }
   };
@@ -20,18 +22,30 @@ function NavBar() {
   const handleFieldChange = (event) => {
     setSearchQuery(event.target.value);
   };
+  const inputSearchFoucs = (val) => {
+    setSearchFocus(val);
+  };
   return (
     <div className={`${hideValue}`}>
       <nav className='navbar'>
         <input
           type='search'
-          className='navbar__search'
+          className='navbar__search dropdown-toggle'
           placeholder='Search for animes'
           name='searchQuery'
           autoComplete='off'
           onKeyPress={handleClickOnSearch}
           onChange={handleFieldChange}
+          onFocus={() => inputSearchFoucs('focused')}
+          onBlur={() => inputSearchFoucs('')}
         />
+        <ul className={`list-group navbar__search__list ${searchFoucs}`}>
+          <li className='list-group-item navbar__search__list__item'>An item</li>
+          <li className='list-group-item navbar__search__list__item'>A second item</li>
+          <li className='list-group-item navbar__search__list__item'>A third item</li>
+          <li className='list-group-item navbar__search__list__item'>A fourth item</li>
+          <li className='list-group-item navbar__search__list__item'>And a fifth one</li>
+        </ul>
         {isLoggedIn() ? (
           <div className='navbar__buttons--login'>
             <Link to='/profile'>
